596a0f9e6a24e225dcc0d6891ec9af3c045d1e37
Bismarck Sevilla <bismarck.storrez@gmail.com>
2021-08-27 23:07:49 -0600

    Defining components

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Sidebar from './components/Sidebar'
+
+
+const Home = () => (
+	<div className='home-page'>
+		<Sidebar />
+	</div>
+)
+
+export default Home
---
+++ src/pages/components/Sidebar.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './sidebar.scss'
+
+const Sidebar = () => (
+		<div className="sidebar-content">
+			<h2>Sidebar</h2>
+		</div>
+	)
+
+export default Sidebar
