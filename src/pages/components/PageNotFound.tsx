91b9cb2b2f6b3c882237a9d0101354709f3e0940
Bismarck Sevilla <bismarck.storrez@gmail.com>
2021-08-29 23:00:51 -0600

    Styling pages

--- src/pages/components/PageNotFound.tsx
+++ src/pages/components/PageNotFound.tsx
@@ -1,9 +1,15 @@
-import React from "react"
+import React from 'react';
 
 const PageNotFound = () => (
-    <div>
-      <h1>404</h1>
-    </div>
-  )
+	<div className='not-found'>
+		<div className='container'>
+			<div className='row'>
+				<div className='col-12'>
+					<h1>404</h1>
+				</div>
+			</div>
+		</div>
+	</div>
+);
 
-export default PageNotFound
+export default PageNotFound;
